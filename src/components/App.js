4d225179493430fdd6e20d5747e6906ea12f3902
Isaiah Odhner <isaiahodhner@gmail.com>
2018-05-15 00:51:24 -0400

    Remove Firebase login

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,6 @@ import IconButton from 'material-ui/IconButton';
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
 import FlatButton from 'material-ui/FlatButton';
-import Login from './Login.js';
-import User from './User.js';
 import DocumentTitle from './DocumentTitle.js';
 import DrawingCanvas from './DrawingCanvas.js';
 import Toolbox from './Toolbox.js';
@@ -16,30 +14,11 @@ import tools from '../tools/';
 import './App.css';
 
 const {firebase} = window;
-const auth = firebase.auth();
-
-const UserMenu = (props) => {
-	const {authData, signOut} = props;
-	return (
-		<IconMenu
-			iconButtonElement={
-				<IconButton style={{width: 50, height: 50, padding: 10}}>
-					<User authData={authData} />
-				</IconButton>
-			}
-			targetOrigin={{horizontal: 'right', vertical: 'top'}}
-			anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-		>
-			<MenuItem primaryText="Sign out" onTouchTap={signOut} />
-		</IconMenu>
-	);
-};
 
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			authData: null,
 			selectedSwatch: defaultPalette[0],
 			selectedTool: tools[0],
 			undos: new List(),
@@ -51,9 +30,6 @@ class App extends Component {
 		this.documentCanvas.height = 480;
 	}
 	componentDidMount() {
-		this.unsubscribeAuthStateChanged = auth.onAuthStateChanged((authData)=> {
-			this.setState({authData});
-		});
 		window.addEventListener("keydown", this.keyDownListener = (event)=> {
 			if (event.ctrlKey) {
 				if (event.key === "z") {
@@ -67,7 +43,6 @@ class App extends Component {
 		});
 	}
 	componentWillUnmount() {
-		this.unsubscribeAuthStateChanged();
 		window.removeEventListener("keydown", this.keyDownListener);
 	}
 	componentWillReceiveProps(nextProps) {
@@ -112,20 +87,14 @@ class App extends Component {
 
 	render() {
 		const {documentID, goToDocument, createNewDocument} = this.props;
-		const {authData, selectedSwatch, selectedTool} = this.state;
+		const {selectedSwatch, selectedTool} = this.state;
 
-		if (!authData) {
-			return <Login />;
-		}
-		const signOut = ()=> {
-			auth.signOut();
-		};
 		const documentsRef = firebase.database().ref("documents");
 		const documentRef = documentsRef.child(documentID);
 		const documentTitleRef = documentRef.child("title");
 
 		const createAndGoToNewDocument = ()=> {
-			createNewDocument(documentsRef, authData.uid, (err, newDocumentID)=> {
+			createNewDocument(documentsRef, "user id here 23049803948029384", (err, newDocumentID)=> {
 				if (err) {
 					// TODO: visible error
 					console.error('Failed to create new document', err);
@@ -158,9 +127,6 @@ class App extends Component {
 					<ToolbarGroup firstChild={true}>
 						<FlatButton onClick={createAndGoToNewDocument} label="New Document"/>
 						<DocumentTitle documentTitleRef={documentTitleRef} />
-					</ToolbarGroup>
-					<ToolbarGroup>
-						<UserMenu authData={authData} signOut={signOut} />
 					</ToolbarGroup>
 				</Toolbar>
 				<main>
