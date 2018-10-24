cebfe3c548bdbbdc6ff9b3a98a809568c4766855
Isaiah Odhner <isaiahodhner@gmail.com>
2018-10-24 19:09:24 -0400

    Add pseudo loading indicators to history view

--- src/components/HistoryEntry.js
+++ src/components/HistoryEntry.js
@@ -4,15 +4,99 @@ import PropTypes from "prop-types";
 import ToolPreview from "./ToolPreview.js";
 import "./HistoryEntry.css";
 
+// const modulo = (a, b) => (+a % (b = +b) + b) % b;
+
 class Thumbnail extends Component {
 	render() {
 		const { width, height } = this.props;
+		this.props.drawFunctionsArrayToAddTo.push(() => this.draw());
+		this.maybeLoadingTime = 0;
 		return <canvas width={width} height={height} />;
 	}
 	draw() {
-		var canvas = ReactDOM.findDOMNode(this);
-		var ctx = canvas.getContext("2d");
-		ctx.drawImage(this.props.image, 0, 0, canvas.width, canvas.height);
+		const { indexInListForAnimationOffset } = this.props;
+		const canvas = ReactDOM.findDOMNode(this);
+		const ctx = canvas.getContext("2d");
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		if (this.props.image) {
+			ctx.drawImage(this.props.image, 0, 0, canvas.width, canvas.height);
+		} else {
+			// ctx.fillRect(Math.random()*canvas.width / 2, Math.random()*canvas.height / 2, 10, 10);
+			/*
+			ctx.beginPath();
+			const baseRotationTurns =
+				Math.sin(
+					performance.now() / 1000 +
+					indexInListForAnimationOffset * 0.03
+					// 0
+				) * 5 / Math.pow(1.001, this.maybeLoadingTime++);
+			const baseRotationRadians = modulo(baseRotationTurns * Math.PI * 2, Math.PI * 2);
+			ctx.arc(
+				canvas.width / 2,
+				canvas.height / 2,
+				Math.min(canvas.width, canvas.height) / 2 * 0.9,
+				baseRotationRadians,
+				baseRotationRadians + Math.PI / 2,
+				false
+			);
+			ctx.lineWidth = 2;
+			ctx.strokeStyle = "rgba(128, 128, 128, 0.8)";
+			ctx.stroke();
+			*/
+			// ctx.save();
+			for (let i = 0; i < 4; i += 1) {
+				let radius =
+					// (Math.min(canvas.width, canvas.height) / 2 - 5) *
+					((Math.min(canvas.width, canvas.height) / 2) *
+						(2 +
+							Math.sin(
+								(i * Math.PI) / 2 +
+									performance.now() / 1000 -
+									indexInListForAnimationOffset * 0.03
+							))) /
+					3;
+				// ctx.lineWidth = 5 *
+				// 	(1 + Math.sin(Math.PI/2 +
+				// 		i * Math.PI/2 +
+				// 		performance.now() / 1000 +
+				// 		indexInListForAnimationOffset * 0.03
+				// 	)) / 2;
+				// radius += ctx.lineWidth / 2;
+				// ctx.lineWidth = radius / 2;
+				// radius += ctx.lineWidth / 2;
+
+				// ctx.strokeStyle = "rgba(128, 128, 128, 0.8)";
+				// ctx.fillStyle = "rgba(128, 128, 128, 0.8)";
+				ctx.fillStyle = `rgba(128, 128, 128, ${0.8 *
+					Math.max(
+						0,
+						Math.sin(
+							Math.PI / 2 +
+								(i * Math.PI) / 2 +
+								performance.now() / 1000 -
+								indexInListForAnimationOffset * 0.03
+						)
+					)})`;
+				ctx.beginPath();
+				ctx.arc(
+					canvas.width / 2,
+					canvas.height / 2,
+					radius,
+					0,
+					Math.PI * 2,
+					false
+				);
+				// ctx.stroke();
+				ctx.fill();
+			}
+			// ctx.restore();
+			ctx.fillStyle = "white";
+			ctx.textBaseline = "middle";
+			ctx.textAlign = "center";
+			ctx.font = `${(Math.min(canvas.width, canvas.height) / 2) *
+				0.9}px sans-serif`;
+			ctx.fillText("?", canvas.width / 2, canvas.height / 2);
+		}
 	}
 	componentDidMount() {
 		this.draw();
@@ -28,8 +112,13 @@ class Thumbnail extends Component {
 
 class HistoryEntry extends Component {
 	render() {
-		const { selected, onClick, entry } = this.props;
-		// TODO: always show thumbnail, have loading state (or other indicator) if not available
+		const {
+			selected,
+			onClick,
+			entry,
+			indexInListForAnimationOffset,
+			drawFunctionsArrayToAddTo,
+		} = this.props;
 		return (
 			<button
 				className="HistoryEntry"
@@ -37,9 +126,13 @@ class HistoryEntry extends Component {
 				aria-checked={selected ? "aria-checked" : null}
 				onClick={onClick}
 			>
-				{entry.thumbnail && (
-					<Thumbnail image={entry.thumbnail} width={24} height={24} />
-				)}
+				<Thumbnail
+					image={entry.thumbnail}
+					width={24}
+					height={24}
+					indexInListForAnimationOffset={indexInListForAnimationOffset}
+					drawFunctionsArrayToAddTo={drawFunctionsArrayToAddTo}
+				/>
 				<ToolPreview tool={entry.tool} width={16} height={16} />
 				{entry.tool.name}
 			</button>
@@ -55,7 +148,7 @@ class HistoryEntry extends Component {
 		}
 	}
 	scrollIntoView() {
-		var el = ReactDOM.findDOMNode(this);
+		const el = ReactDOM.findDOMNode(this);
 		el.scrollIntoView({
 			behavior: "instant",
 			block: "nearest",
--- src/components/HistoryView.js
+++ src/components/HistoryView.js
@@ -1,5 +1,5 @@
 import { List } from "immutable";
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import HistoryEntry from "./HistoryEntry.js";
 import "./HistoryView.css";
@@ -7,27 +7,47 @@ import "./HistoryView.css";
 // TODO: DRY Toolbox + Palette + HistoryView maybe
 // should refactor it so the list is separate from the history entry display!
 // and support keyboard navigation! and scroll the view
-const HistoryView = (props) => {
-	const { undos, redos, goToEntry } = props;
-	const allHistory = undos.concat(redos.reverse());
-	// TODO: should have an entry for New Document
-	// so you can click to before the first operation, like you can Ctrl+Z to it!
-	return (
-		<div className="HistoryView" role="radiogroup">
-			{allHistory.map((entry, index) => {
-				const selected = entry === undos.last();
-				return (
-					<HistoryEntry
-						key={entry.id}
-						entry={entry}
-						selected={selected}
-						onClick={() => goToEntry(entry)}
-					/>
-				);
-			})}
-		</div>
-	);
-};
+class HistoryView extends Component {
+	constructor(props) {
+		super();
+		// TODO: cleanup how this works!
+		this.drawFunctions = [];
+	}
+	componentDidMount() {
+		const animate = () => {
+			this.animationFrameID = requestAnimationFrame(animate);
+			this.drawFunctions.forEach((fn) => fn());
+		};
+		animate();
+	}
+	componentWillUnmount() {
+		cancelAnimationFrame(this.animationFrameID);
+	}
+	render() {
+		this.drawFunctions = [];
+		// TODO: should have an entry for New Document
+		// so you can click to before the first operation, like you can Ctrl+Z to it!
+		const { undos, redos, goToEntry } = this.props;
+		const allHistory = undos.concat(redos.reverse());
+		return (
+			<div className="HistoryView" role="radiogroup">
+				{allHistory.map((entry, index) => {
+					const selected = entry === undos.last();
+					return (
+						<HistoryEntry
+							key={entry.id}
+							entry={entry}
+							selected={selected}
+							onClick={() => goToEntry(entry)}
+							indexInListForAnimationOffset={index}
+							drawFunctionsArrayToAddTo={this.drawFunctions}
+						/>
+					);
+				})}
+			</div>
+		);
+	}
+}
 
 HistoryView.propTypes = {
 	undos: PropTypes.instanceOf(List).isRequired,
