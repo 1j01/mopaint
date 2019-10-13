1391823bf6fbb76818216d76191a1863fdc43611
Isaiah Odhner <isaiahodhner@gmail.com>
2019-10-13 11:26:37 -0400

    Sketch overlaying on page

---
+++ src/engine.js
@@ -0,0 +1,54 @@
+/*
+export function render(mopaintDocument, canvas, inputs) {
+	const ctx = canvas.getContext("2d");
+
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	const runningHash = sha256();
+	mopaintDocument.operations.forEach((operation, operationIndex) => {
+		runningHash.update(JSON.stringify(operation));
+		const operationHash = runningHash.digest("hex");
+
+		operation.exec(ctx);
+		
+		this.hashInDocumentByOperation.set(operation, operationHash);
+	});
+
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.drawImage(documentCanvas, 0, 0);
+}
+
+export function overlayOnPage(mopaintDocument, elements) {
+	const canvas = document.createElement("canvas");
+
+	const makeFrameInputs = ()=> {
+		const rects = {};
+		for (let id, element of elements) {
+			const {left, top, right, bottom} = element.getBoundingClientRect();
+			const x = left, y = top, width = right - left, height = bottom - top;
+			rects[id] = {x, y, width, height};
+		}
+		const pageWidth = innerWidth, pageHeight = innerHeight;
+		const inputs = {rects, pageWidth, pageHeight};
+		return inputs;
+	};
+
+	canvas.style.pointerEvents = "none";
+	canvas.style.position = "fixed";
+	canvas.style.left = "0";
+	canvas.style.top = "0";
+	document.body.appendChild(canvas);
+	
+	const animate = ()=> {
+		const inputs = makeFrameInputs();
+
+		canvas.width = inputs.pageWidth;
+		canvas.height = inputs.pageHeight;
+
+		render(mopaintDocument, canvas, inputs);
+
+		requestAnimationFrame(animate);
+	};
+	requestAnimationFrame(animate);
+}
+*/
