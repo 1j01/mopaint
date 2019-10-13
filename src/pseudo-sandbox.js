05e60fadcf64f958a9bcbe2ad0ef26a18b78cfcf
Isaiah Odhner <isaiahodhner@gmail.com>
2019-10-13 12:47:45 -0400

    async
    and check for localStorage value to be specifically "true" so that if you set it to a value like false which becomes "false" it's not treated as truthy

--- src/pseudo-sandbox.js
+++ src/pseudo-sandbox.js
@@ -6,13 +6,12 @@ function hexString(buffer) {
 	return hexCodes.join("");
 }
 
-export function hash(message) {
+export async function hash(message) {
 	const encoder = new TextEncoder();
 	const data = encoder.encode(message);
-	return window.crypto.subtle.digest("SHA-256", data).then((digestValue)=> {
-		const digestHex = hexString(digestValue);
-		return digestHex;
-	});
+	const digestValue = await window.crypto.subtle.digest("SHA-256", data);
+	const digestHex = hexString(digestValue);
+	return digestHex;
 }
 
 window.digestHexesToMaybeTrust = [];
@@ -20,15 +19,18 @@ const trustedDigestHexes = [
 	"8c783d1d71013d93bc6667e6af3001fa3c32cbbcbf2e067771baf2ee015f95e4"
 ];
 
-export default function importModuleFromCodeIfTrusted(code) {
-	return hash(code).then(digestHex => {
-		window.digestHexesToMaybeTrust.push(digestHex);
-		const allowed = trustedDigestHexes.includes(digestHex) || (()=> { try { return localStorage.disableSecurityAndTrustAnything } catch (e) {} })();
-		console.log(allowed);
-		if (allowed) {
-			return importModuleFromCode(code);
-		} else {
-			throw new Error(`Untrusted code. (Digest '${digestHex}' is not in the list of allowed digests.)`);
-		}
-	});
+export default async function importModuleFromCodeIfTrusted(code) {
+	const digestHex = await hash(code);
+	window.digestHexesToMaybeTrust.push(digestHex);
+	const allowed = trustedDigestHexes.includes(digestHex) || (() => {
+		try {
+			return localStorage.disableSecurityAndTrustAnything === "true";
+		} catch (e) { }
+	})();
+	console.log(allowed);
+	if (allowed) {
+		return importModuleFromCode(code);
+	} else {
+		throw new Error(`Untrusted code. (Digest '${digestHex}' is not in the list of allowed digests.)`);
+	}
 };
