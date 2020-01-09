1fd1c7556c9fde669bf716399106fd75bb58805d
Isaiah Odhner <isaiahodhner@gmail.com>
2020-01-09 09:12:43 -0500

    Refactor: pull out draw into engine

--- src/components/DrawingCanvas.js
+++ src/components/DrawingCanvas.js
@@ -1,9 +1,9 @@
 // TODO: evaluate other hashing libraries
 // https://brillout.github.io/test-javascript-hash-implementations/
-import sha256 from "hash.js/lib/hash/sha/256";
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import "./DrawingCanvas.css";
+import {draw} from "../engine.js";
 
 // TODO maybe: more efficient IDs (don't need to restrict the alphabet to hex)
 const byteToHex = (byte) => `0${byte.toString(16)}`.slice(-2);
@@ -38,7 +38,17 @@ class DrawingCanvas extends Component {
 		if (this.animationFrameID) {
 			cancelAnimationFrame(this.animationFrameID);
 		}
-		this.animationFrameID = requestAnimationFrame(this.draw.bind(this));
+		this.animationFrameID = requestAnimationFrame(()=> {
+			draw({
+				documentCanvas: this.props.documentCanvas,
+				displayCanvas: this.canvasRef.current,
+				opCanvas: this.opCanvas,
+				operations: this.props.operations,
+				thumbnailsByOperation: this.props.thumbnailsByOperation,
+				cache: this.cache,
+				hashInDocumentByOperation: this.hashInDocumentByOperation,
+			});
+		});
 		return (
 			<div className="DrawingCanvas" style={{ width, height }}>
 				<canvas width={width} height={height} ref={this.canvasRef} />
@@ -52,96 +62,6 @@ class DrawingCanvas extends Component {
 			x: event.clientX - rect.left,
 			y: event.clientY - rect.top,
 		};
-	}
-	draw() {
-		const canvas = this.canvasRef.current;
-		const { opCanvas, opCtx } = this;
-		const {
-			documentCanvas,
-			documentContext,
-			operations,
-			thumbnailsByOperation,
-		} = this.props;
-		const ctx = canvas.getContext("2d");
-
-		documentContext.clearRect(0, 0, opCanvas.width, opCanvas.height);
-
-		const runningHash = sha256();
-		operations.forEach((operation, operationIndex) => {
-			runningHash.update(JSON.stringify(operation));
-			const operationHash = runningHash.digest("hex");
-			opCtx.clearRect(0, 0, opCanvas.width, opCanvas.height);
-			if (this.cache[operationHash]) {
-				// console.log("cache hit");
-				documentContext.drawImage(this.cache[operationHash], 0, 0);
-			} else {
-				// opCtx.clearRect(0, 0, opCanvas.width, opCanvas.height);
-				// console.log("cache miss");
-				const { points, tool, swatch } = operation;
-				const startPos = points[0];
-				const lastPos = points[points.length - 1];
-				if (tool.drawShape) {
-					tool.drawShape(
-						opCtx,
-						startPos.x,
-						startPos.y,
-						lastPos.x,
-						lastPos.y,
-						swatch
-					);
-				}
-				if (tool.drawSegmentOfPath) {
-					// TODO: allow for smoothing (rather than just plain segments)
-					for (let i1 = 0, i2 = 1; i2 < points.length; i1 += 1, i2 += 1) {
-						tool.drawSegmentOfPath(
-							opCtx,
-							points[i1].x,
-							points[i1].y,
-							points[i2].x,
-							points[i2].y,
-							swatch
-						);
-					}
-				}
-				if (tool.click) {
-					tool.click(opCtx, startPos.x, startPos.y, swatch, documentContext);
-				}
-
-				documentContext.drawImage(opCanvas, 0, 0);
-
-				this.hashInDocumentByOperation.set(operation, operationHash);
-
-				// fill the cache
-				if (!operation.updatingContinously) {
-					this.cache[operationHash] = document.createElement("canvas");
-					this.cache[operationHash].width = opCanvas.width;
-					this.cache[operationHash].height = opCanvas.height;
-					this.cache[operationHash].getContext("2d").drawImage(opCanvas, 0, 0);
-				}
-
-				// TODO: optimize: use existing canvas if it exists (clear and update it)
-				const thumbnail = document.createElement("canvas");
-				thumbnail.width = 64;
-				thumbnail.height = 64;
-				// TODO: keep thumbnail proportional
-				// can reuse code in ToolPreview.js
-				thumbnail.getContext("2d").drawImage(opCanvas, 0, 0, 64, 64);
-				thumbnailsByOperation.set(operation, thumbnail);
-
-				// invalidate the cache for any future operations
-				// TODO: need to invalidate "redos"
-				// // console.log("invalidate", operations, operationIndex + 1);
-				// operations.slice(operationIndex + 1).forEach((operation)=> {
-				// 	// TODO: unless self-contained
-				// 	// console.log(thumbnailsByOperation.has(operation), thumbnailsByOperation);
-				// 	thumbnailsByOperation.delete(operation);
-				// 	delete this.cache[this.hashInDocumentByOperation.get(operation)];
-				// });
-			}
-		});
-
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		ctx.drawImage(documentCanvas, 0, 0);
 	}
 	// TODO: touch support
 	onMouseMoveWhileDown(event) {
--- src/engine.js
+++ src/engine.js
@@ -1,3 +1,90 @@
+import sha256 from "hash.js/lib/hash/sha/256";
+
+export const draw = ({documentCanvas, displayCanvas, opCanvas, operations, thumbnailsByOperation, cache, hashInDocumentByOperation})=> {
+	const documentContext = documentCanvas.getContext("2d");
+	const opCtx = opCanvas.getContext("2d");
+	const displayCtx = displayCanvas.getContext("2d");
+
+	documentContext.clearRect(0, 0, opCanvas.width, opCanvas.height);
+
+	const runningHash = sha256();
+	operations.forEach((operation, operationIndex) => {
+		runningHash.update(JSON.stringify(operation));
+		const operationHash = runningHash.digest("hex");
+		opCtx.clearRect(0, 0, opCanvas.width, opCanvas.height);
+		if (cache[operationHash]) {
+			// console.log("cache hit");
+			documentContext.drawImage(cache[operationHash], 0, 0);
+		} else {
+			// opCtx.clearRect(0, 0, opCanvas.width, opCanvas.height);
+			// console.log("cache miss");
+			const { points, tool, swatch } = operation;
+			const startPos = points[0];
+			const lastPos = points[points.length - 1];
+			if (tool.drawShape) {
+				tool.drawShape(
+					opCtx,
+					startPos.x,
+					startPos.y,
+					lastPos.x,
+					lastPos.y,
+					swatch
+				);
+			}
+			if (tool.drawSegmentOfPath) {
+				// TODO: allow for smoothing (rather than just plain segments)
+				for (let i1 = 0, i2 = 1; i2 < points.length; i1 += 1, i2 += 1) {
+					tool.drawSegmentOfPath(
+						opCtx,
+						points[i1].x,
+						points[i1].y,
+						points[i2].x,
+						points[i2].y,
+						swatch
+					);
+				}
+			}
+			if (tool.click) {
+				tool.click(opCtx, startPos.x, startPos.y, swatch, documentContext);
+			}
+
+			documentContext.drawImage(opCanvas, 0, 0);
+
+			hashInDocumentByOperation.set(operation, operationHash);
+
+			// fill the cache
+			if (!operation.updatingContinously) {
+				cache[operationHash] = document.createElement("canvas");
+				cache[operationHash].width = opCanvas.width;
+				cache[operationHash].height = opCanvas.height;
+				cache[operationHash].getContext("2d").drawImage(opCanvas, 0, 0);
+			}
+
+			// TODO: optimize: use existing canvas if it exists (clear and update it)
+			const thumbnail = document.createElement("canvas");
+			thumbnail.width = 64;
+			thumbnail.height = 64;
+			// TODO: keep thumbnail proportional
+			// can reuse code in ToolPreview.js
+			thumbnail.getContext("2d").drawImage(opCanvas, 0, 0, 64, 64);
+			thumbnailsByOperation.set(operation, thumbnail);
+
+			// invalidate the cache for any future operations
+			// TODO: need to invalidate "redos"
+			// // console.log("invalidate", operations, operationIndex + 1);
+			// operations.slice(operationIndex + 1).forEach((operation)=> {
+			// 	// TODO: unless self-contained
+			// 	// console.log(thumbnailsByOperation.has(operation), thumbnailsByOperation);
+			// 	thumbnailsByOperation.delete(operation);
+			// 	delete cache[hashInDocumentByOperation.get(operation)];
+			// });
+		}
+	});
+
+	displayCtx.clearRect(0, 0, displayCanvas.width, displayCanvas.height);
+	displayCtx.drawImage(documentCanvas, 0, 0);
+}
+
 /*
 export function render(mopaintDocument, canvas, inputs) {
 	const ctx = canvas.getContext("2d");
