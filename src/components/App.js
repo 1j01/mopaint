1e2a68dfbee761818f7c59590fe9b2ac3a0e3392
Isaiah Odhner <isaiahodhner@gmail.com>
2020-02-01 21:13:22 -0500

    Refactor: move deserialization to document-format.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import PropTypes from "prop-types";
 import { load as loadPalette } from "anypalette";
 import isPNG from "is-png";
 import { injectMetadataIntoBlob, readMetadataSync } from "../png-metadata.js";
-import { serializeDocument, CURRENT_SERIALIZATION_VERSION } from "../document-format"; 
+import { serializeDocument, deserializeDocument } from "../document-format"; 
 import tools, { toolsByName } from "../tools";
 import DrawingCanvas from "./DrawingCanvas.js";
 import Toolbox from "./Toolbox.js";
@@ -62,98 +62,9 @@ class App extends Component {
 	}
 
 	loadSerializedDocument(serialized, fromFile) {
-		const nounPhraseThingToLoad = fromFile ? "document" : "document state";
-		if (serialized.format !== "mopaint") {
-			this.showError({
-				message: `Can't load ${nounPhraseThingToLoad} - it does not appear to be a Mopaint document`,
-			});
-			this.setState({ loadFailed: true });
-			return;
-		}
-		if (
-			typeof serialized.formatVersion !== "number" ||
-			serialized.formatVersion > CURRENT_SERIALIZATION_VERSION
-		) {
-			this.showError({
-				message: `Can't load ${nounPhraseThingToLoad} created by later version of the app`,
-			});
-			this.setState({ loadFailed: true });
-			return;
-		}
-		const MINIMUM_LOADABLE_VERSION = 0.1;
-		// upgrading code can go here, incrementing the version number step by step
-		// e.g.
-		// if (serialized.formatVersion === 0.2) {
-		// 	serialized.newPropName = serialized.oldName;
-		// 	delete serialized.oldName;
-		// 	serialized.formatVersion = 0.3;
-		// }
-		if (serialized.formatVersion === 0.1) {
-			// literally no change afaik, just internals
-			// just skipping over version 0.2, which was a dead end
-			serialized.formatVersion = 0.3;
-		}
-		if (serialized.formatVersion < CURRENT_SERIALIZATION_VERSION) {
-			const gitBranchName = `format-version-${serialized.formatVersion}`;
-			this.showError({
-				message: <React.Fragment>
-					<p>
-						Can't load {nounPhraseThingToLoad} created by old version of the app; there's no upgrade path from format version {
-							serialized.formatVersion
-						} to {CURRENT_SERIALIZATION_VERSION}{
-							MINIMUM_LOADABLE_VERSION !== CURRENT_SERIALIZATION_VERSION
-								? ` (minimum loadable: ${MINIMUM_LOADABLE_VERSION})`
-								: ""
-						}
-					</p>
-					<p>
-						To load this {nounPhraseThingToLoad}, use the version of Mopaint at the Git branch&nbsp;
-						<a href={`https://github.com/1j01/mopaint/tree/${gitBranchName}`} style={{fontFamily: "monospace"}}>{gitBranchName}</a>
-					</p>
-				</React.Fragment>
-			});
-			this.setState({ loadFailed: true });
-			return;
-		}
-	
-		const expectPropertiesToExist = (properties, object, locationMessage) => {
-			properties.forEach((key) => {
-				if (!object[key]) {
-					throw new TypeError(`expected property '${key}' ${locationMessage}`);
-				}
-			});
-		};
-		const deserializeOperation = (serializedOperation) => {
-			expectPropertiesToExist(
-				["id", "toolID", "points", "swatch"],
-				serializedOperation,
-				`on operation with ID ${serializedOperation.id}`,
-			);
-			const tool = getToolByName(serializedOperation.toolID);
-			return {
-				id: serializedOperation.id,
-				tool: tool,
-				points: serializedOperation.points,
-				swatch: serializedOperation.swatch,
-			};
-		};
-		// this try-catch is mainly for the explicitly thrown TypeErrors,
-		// but TODO: maybe wrap all the loading logic, and maybe saving too
-		let stateUpdates;
+		let errorInfo, stateUpdates;
 		try {
-			expectPropertiesToExist(
-				["palette", "selectedSwatch", "selectedToolID", "undos", "redos"],
-				serialized,
-				"on the root document object",
-			);
-			stateUpdates = {
-				palette: serialized.palette,
-				selectedSwatch: serialized.selectedSwatch,
-				selectedTool: getToolByName(serialized.selectedToolID),
-				undos: new List(serialized.undos.map(deserializeOperation)),
-				redos: new List(serialized.redos.map(deserializeOperation)),
-				loaded: true,
-			};
+			[errorInfo, stateUpdates] = deserializeDocument(serialized, fromFile, getToolByName);
 		} catch (error) {
 			this.showError({
 				message: "Failed to load document!",
@@ -161,9 +72,15 @@ class App extends Component {
 			});
 			this.setState({ loadFailed: true });
 		}
-		this.setState(stateUpdates, () => {
-			console.log(`Loaded ${this.props.documentID}`);
-		});
+		if (errorInfo) {
+			this.showError(errorInfo);
+			this.setState({ loadFailed: true });
+		} else {
+			stateUpdates.loaded = true;
+			this.setState(stateUpdates, () => {
+				console.log(`Loaded ${this.props.documentID}`);
+			});
+		}
 	}
 
 	load() {
--- src/document-format.js
+++ src/document-format.js
@@ -1,3 +1,5 @@
+import React from "react";
+import { List } from "immutable";
 
 export const CURRENT_SERIALIZATION_VERSION = 0.3;
 
@@ -7,7 +9,6 @@ export function serializeDocument({palette, selectedSwatch, selectedTool, undos,
 		return {
 			id: operation.id,
 			toolID: operation.tool.name,
-			// toolCode: operation.tool.toString(), // not enough to define it; will need the whole module
 			points: operation.points,
 			swatch: operation.swatch,
 		};
@@ -23,6 +24,86 @@ export function serializeDocument({palette, selectedSwatch, selectedTool, undos,
 	};
 }
 
-export function deserializeDocument() {
+export function deserializeDocument(serialized, isFromFile, getToolByName) {
+	const nounPhraseThingToLoad = isFromFile ? "document" : "document state";
+	if (serialized.format !== "mopaint") {
+		return [{
+			message: `Can't load ${nounPhraseThingToLoad} - it does not appear to be a Mopaint document`,
+		}];
+	}
+	if (
+		typeof serialized.formatVersion !== "number" ||
+		serialized.formatVersion > CURRENT_SERIALIZATION_VERSION
+	) {
+		return [{
+			message: `Can't load ${nounPhraseThingToLoad} created by later version of the app`,
+		}];
+	}
+	const MINIMUM_LOADABLE_VERSION = 0.1;
+	// upgrading code can go here, incrementing the version number step by step
+	// e.g.
+	// if (serialized.formatVersion === 0.2) {
+	// 	serialized.newPropName = serialized.oldName;
+	// 	delete serialized.oldName;
+	// 	serialized.formatVersion = 0.3;
+	// }
+	if (serialized.formatVersion === 0.1) {
+		// literally no change afaik, just internals
+		// just skipping over version 0.2, which was a dead end
+		serialized.formatVersion = 0.3;
+	}
+	if (serialized.formatVersion < CURRENT_SERIALIZATION_VERSION) {
+		const gitBranchName = `format-version-${serialized.formatVersion}`;
+		return [{
+			message: <React.Fragment>
+				<p>
+					Can't load {nounPhraseThingToLoad} created by old version of the app; there's no upgrade path from format version {
+						serialized.formatVersion
+					} to {CURRENT_SERIALIZATION_VERSION}{
+						MINIMUM_LOADABLE_VERSION !== CURRENT_SERIALIZATION_VERSION
+							? ` (minimum loadable: ${MINIMUM_LOADABLE_VERSION})`
+							: ""
+					}
+				</p>
+				<p>
+					To load this {nounPhraseThingToLoad}, use the version of Mopaint at the Git branch&nbsp;
+					<a href={`https://github.com/1j01/mopaint/tree/${gitBranchName}`} style={{fontFamily: "monospace"}}>{gitBranchName}</a>
+				</p>
+			</React.Fragment>
+		}];
+	}
 
+	const expectPropertiesToExist = (properties, object, locationMessage) => {
+		properties.forEach((key) => {
+			if (!object[key]) {
+				throw new TypeError(`expected property '${key}' ${locationMessage}`);
+			}
+		});
+	};
+	const deserializeOperation = (serializedOperation) => {
+		expectPropertiesToExist(
+			["id", "toolID", "points", "swatch"],
+			serializedOperation,
+			`on operation with ID ${serializedOperation.id}`,
+		);
+		const tool = getToolByName(serializedOperation.toolID);
+		return {
+			id: serializedOperation.id,
+			tool: tool,
+			points: serializedOperation.points,
+			swatch: serializedOperation.swatch,
+		};
+	};
+	expectPropertiesToExist(
+		["palette", "selectedSwatch", "selectedToolID", "undos", "redos"],
+		serialized,
+		"on the root document object",
+	);
+	return [null, {
+		palette: serialized.palette,
+		selectedSwatch: serialized.selectedSwatch,
+		selectedTool: getToolByName(serialized.selectedToolID),
+		undos: new List(serialized.undos.map(deserializeOperation)),
+		redos: new List(serialized.redos.map(deserializeOperation)),
+	}];
 }
