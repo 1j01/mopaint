1j01/mopaint

commit 8b551d446791d2ee8737150fc427eb4b907ba09f

Add icon to New Document entry

src/components/HistoryEntry.js
@@ -133,6 +133,7 @@ class HistoryEntry extends Component {
 			indexInListForAnimationOffset,
 			drawFunctionsArrayToAddTo,
 			getThumbnailImageMaybe,
+			getThumbnailReactElementMaybe,
 		} = this.props;
 		const {operation} = historyNode;
 		// NOTE: className, role, and aria-checked are all relied upon in HistoryView
@@ -144,13 +145,15 @@ class HistoryEntry extends Component {
 				onClick={onClick} // for keyboard accessibility
 				onMouseDown={onClick} // for speed (w/ a mouse)
 			>
-				<Thumbnail
-					width={24}
-					height={24}
-					indexInListForAnimationOffset={indexInListForAnimationOffset}
-					drawFunctionsArrayToAddTo={drawFunctionsArrayToAddTo}
-					getImageMaybe={getThumbnailImageMaybe}
-				/>
+				{getThumbnailReactElementMaybe?.() ??
+					<Thumbnail
+						width={24}
+						height={24}
+						indexInListForAnimationOffset={indexInListForAnimationOffset}
+						drawFunctionsArrayToAddTo={drawFunctionsArrayToAddTo}
+						getImageMaybe={getThumbnailImageMaybe}
+					/>
+				}
 				{operation && <ToolPreview tool={operation.tool} width={16} height={16} />}
 				{historyNode.name}
 			</button>

src/components/HistoryView.js
@@ -5,6 +5,7 @@ import HistoryEntry from "./HistoryEntry.js";
 import "./HistoryView.css";
 import {getHistoryAncestors, getAllHistoryNodesSortedByTimestamp} from "../history.js";
 import HistoryNode from "../HistoryNode.js";
+import { ReactComponent as NewDocumentIcon } from "../icons/small-n-flat/document-new-importable.svg";
 
 // TODO: DRY Toolbox + Palette + HistoryView maybe
 // should refactor it so the list is separate from the history entry display!
@@ -91,7 +92,12 @@ class HistoryView extends Component {
 						onClick={() => goToHistoryNode(node)}
 						indexInListForAnimationOffset={0 /* TODO */}
 						drawFunctionsArrayToAddTo={this.drawFunctions}
-						getThumbnailImageMaybe={() => node.operation && thumbnailsByOperation.get(node.operation)}
+						getThumbnailImageMaybe={() =>
+							node.operation && thumbnailsByOperation.get(node.operation)
+						}
+						getThumbnailReactElementMaybe={() =>
+							node.name === "New Document" && <NewDocumentIcon width={16} height={16}/>
+						}
 					/>;
 				})}
 			</div>

src/components/HistoryEntry.css
@@ -23,6 +23,7 @@
 	color: gray;
 }
 .HistoryEntry canvas,
+.HistoryEntry svg,
 .HistoryEntry .ToolPreview {
 	margin-right: 5px;
 	vertical-align: middle;

commit b5420d90b269960b689be34c5ed2975b60bb4a54

Fix thumbnails
Broken in 8b551d446791d2ee8737150fc427eb4b907ba09f "Add icon to New Document entry"

src/components/HistoryEntry.js
@@ -133,7 +133,7 @@ class HistoryEntry extends Component {
 			indexInListForAnimationOffset,
 			drawFunctionsArrayToAddTo,
 			getThumbnailImageMaybe,
-			getThumbnailReactElementMaybe,
+			getIconReactElementMaybe,
 		} = this.props;
 		const {operation} = historyNode;
 		// NOTE: role works together with role in HistoryView
@@ -149,16 +149,14 @@ class HistoryEntry extends Component {
 				onClick={onClick} // for keyboard accessibility (?)
 				onMouseDown={onClick} // for speed (w/ a mouse)
 			>
-				{getThumbnailReactElementMaybe?.() ??
-					<Thumbnail
-						width={24}
-						height={24}
-						indexInListForAnimationOffset={indexInListForAnimationOffset}
-						drawFunctionsArrayToAddTo={drawFunctionsArrayToAddTo}
-						getImageMaybe={getThumbnailImageMaybe}
-					/>
-				}
-				{operation && <ToolPreview tool={operation.tool} width={16} height={16} />}
+				<Thumbnail
+					width={24}
+					height={24}
+					indexInListForAnimationOffset={indexInListForAnimationOffset}
+					drawFunctionsArrayToAddTo={drawFunctionsArrayToAddTo}
+					getImageMaybe={getThumbnailImageMaybe}
+				/>
+				{getIconReactElementMaybe?.() ?? (operation && <ToolPreview tool={operation.tool} width={16} height={16} />)}
 				{historyNode.name}
 			</button>
 		);

src/components/HistoryView.js
@@ -7,6 +7,10 @@ import {getHistoryAncestors, getAllHistoryNodesSortedByTimestamp} from "../histo
 import HistoryNode from "../HistoryNode.js";
 import { ReactComponent as NewDocumentIcon } from "../icons/small-n-flat/document-new-importable.svg";
 
+const blankImage = new Image();
+// transparent single-pixel PNG
+blankImage.src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=";
+
 // TODO: keyboard navigation
 class HistoryView extends Component {
 	constructor(props) {
@@ -75,10 +79,12 @@ class HistoryView extends Component {
 						indexInListForAnimationOffset={0 /* TODO */}
 						drawFunctionsArrayToAddTo={this.drawFunctions}
 						getThumbnailImageMaybe={() =>
-							node.operation && thumbnailsByOperation.get(node.operation)
+							node.operation ?
+								thumbnailsByOperation.get(node.operation) :
+								blankImage
 						}
-						getThumbnailReactElementMaybe={() =>
-							node.name === "New Document" && <NewDocumentIcon width={16} height={16}/>
+						getIconReactElementMaybe={() =>
+							node.name === "New Document" ? <NewDocumentIcon width={16} height={16}/> : null
 						}
 					/>;
 				})}