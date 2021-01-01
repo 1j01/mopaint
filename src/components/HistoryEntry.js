45f2ff97d9387242c0764683989294144fa416c6
Isaiah Odhner <isaiahodhner@gmail.com>
2021-01-01 17:18:20 -0500

    Fix animated question mark (reimplement as dynamically generated spritesheet)

--- src/components/HistoryEntry.js
+++ src/components/HistoryEntry.js
@@ -17,12 +17,11 @@ function isScrolledIntoView(el) {
 class Thumbnail extends Component {
 	render() {
 		const { width, height } = this.props;
-		this.props.drawFunctionsArrayToAddTo.push(() => this.draw());
 		this.maybeLoadingTime = 0;
 		return <canvas width={width} height={height} />;
 	}
 	draw() {
-		const { indexInListForAnimationOffset, getImageMaybe } = this.props;
+		const { indexInListForAnimationOffset, image } = this.props;
 		const canvas = ReactDOM.findDOMNode(this);
 
 		if(!isScrolledIntoView(canvas)){
@@ -31,90 +30,8 @@ class Thumbnail extends Component {
 
 		const ctx = canvas.getContext("2d");
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-		// TODO: clean up how this works!
-		// this getImageMaybe thing is a hack to update the image outside of React's render model,
-		// since we already have an animation loop
-		const image = getImageMaybe();
-		if (image) {
-			ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-		} else {
-			// ctx.fillRect(Math.random()*canvas.width / 2, Math.random()*canvas.height / 2, 10, 10);
-			/*
-			ctx.beginPath();
-			const baseRotationTurns =
-				Math.sin(
-					performance.now() / 1000 +
-					indexInListForAnimationOffset * 0.03
-					// 0
-				) * 5 / Math.pow(1.001, this.maybeLoadingTime++);
-			const baseRotationRadians = modulo(baseRotationTurns * Math.PI * 2, Math.PI * 2);
-			ctx.arc(
-				canvas.width / 2,
-				canvas.height / 2,
-				Math.min(canvas.width, canvas.height) / 2 * 0.9,
-				baseRotationRadians,
-				baseRotationRadians + Math.PI / 2,
-				false
-			);
-			ctx.lineWidth = 2;
-			ctx.strokeStyle = "rgba(128, 128, 128, 0.8)";
-			ctx.stroke();
-			*/
-			// ctx.save();
-			for (let i = 0; i < 4; i += 1) {
-				let radius =
-					// (Math.min(canvas.width, canvas.height) / 2 - 5) *
-					((Math.min(canvas.width, canvas.height) / 2) *
-						(2 +
-							Math.sin(
-								(i * Math.PI) / 2 +
-									performance.now() / 1000 -
-									indexInListForAnimationOffset * 0.03
-							))) /
-					3;
-				// ctx.lineWidth = 5 *
-				// 	(1 + Math.sin(Math.PI/2 +
-				// 		i * Math.PI/2 +
-				// 		performance.now() / 1000 +
-				// 		indexInListForAnimationOffset * 0.03
-				// 	)) / 2;
-				// radius += ctx.lineWidth / 2;
-				// ctx.lineWidth = radius / 2;
-				// radius += ctx.lineWidth / 2;
-
-				// ctx.strokeStyle = "rgba(128, 128, 128, 0.8)";
-				// ctx.fillStyle = "rgba(128, 128, 128, 0.8)";
-				ctx.fillStyle = `rgba(128, 128, 128, ${0.8 *
-					Math.max(
-						0,
-						Math.sin(
-							Math.PI / 2 +
-								(i * Math.PI) / 2 +
-								performance.now() / 1000 -
-								indexInListForAnimationOffset * 0.03
-						)
-					)})`;
-				ctx.beginPath();
-				ctx.arc(
-					canvas.width / 2,
-					canvas.height / 2,
-					radius,
-					0,
-					Math.PI * 2,
-					false
-				);
-				// ctx.stroke();
-				ctx.fill();
-			}
-			// ctx.restore();
-			ctx.fillStyle = "white";
-			ctx.textBaseline = "middle";
-			ctx.textAlign = "center";
-			ctx.font = `${(Math.min(canvas.width, canvas.height) / 2) *
-				0.9}px sans-serif`;
-			ctx.fillText("?", canvas.width / 2, canvas.height / 2);
-		}
+	
+		ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
 	}
 	componentDidMount() {
 		this.draw();
@@ -132,7 +49,7 @@ Thumbnail.propTypes = {
 	width: PropTypes.number.isRequired,
 	height: PropTypes.number.isRequired,
 	indexInListForAnimationOffset: PropTypes.number.isRequired,
-	getImageMaybe: PropTypes.func.isRequired,
+	image: PropTypes.object.isRequired,
 };
 
 class HistoryEntry extends Component {
@@ -151,11 +68,20 @@ class HistoryEntry extends Component {
 			onClick,
 			historyNode,
 			indexInListForAnimationOffset,
-			drawFunctionsArrayToAddTo,
 			getThumbnailImageMaybe,
 			getIconReactElementMaybe,
 		} = this.props;
 		const {operation} = historyNode;
+		const thumbnailImage = getThumbnailImageMaybe();
+		const thumbnail = thumbnailImage ? <Thumbnail
+			width={24}
+			height={24}
+			indexInListForAnimationOffset={indexInListForAnimationOffset}
+			image={thumbnailImage}
+		/> : <div
+			className="question-mark"
+			style={{width: 24, height: 24}}
+		/>;
 		// NOTE: role works together with role in HistoryView
 		return (
 			<button
@@ -169,13 +95,7 @@ class HistoryEntry extends Component {
 				onClick={onClick} // for keyboard accessibility (?)
 				onMouseDown={onClick} // for speed (w/ a mouse)
 			>
-				<Thumbnail
-					width={24}
-					height={24}
-					indexInListForAnimationOffset={indexInListForAnimationOffset}
-					drawFunctionsArrayToAddTo={drawFunctionsArrayToAddTo}
-					getImageMaybe={getThumbnailImageMaybe}
-				/>
+				{thumbnail}
 				{getIconReactElementMaybe?.() ?? (operation && <ToolPreview tool={operation.tool} width={16} height={16} />)}
 				{historyNode.name}
 			</button>
