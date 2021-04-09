c43e7eccfaadb9bbe13b90a948727a08eddb0d65
Isaiah Odhner <isaiahodhner@gmail.com>
2021-04-08 22:46:01 -0400

    Here's my idea for how metahistory can work

--- src/engine.test.js
+++ src/engine.test.js
@@ -1,10 +1,65 @@
 
-function compute({ program, cache, }) {
+function resolveMetaHistory(metaHistory) {
+
+	const maxMHI = metaHistory.reduce((maxMHI, op) => Math.max(maxMHI, op.mhi), 0);
+	const mutableMH = JSON.parse(JSON.stringify(metaHistory));
+
+	// Note: skipping mhi of 0, at that point it should be resolved. (most loops of this type you'd want >= 0)
+	for (let mhi = maxMHI; mhi > 0; mhi--) {
+		for (const op of mutableMH) {
+			if (op.mhi === mhi) {
+				// console.log(op, mhi);
+				// TODO: handle all op types?
+				// TODO: make sure it matches a target, and the metahistory index of the target is less, etc.
+				if (op.type === "undo") {
+					for (const otherOp of mutableMH) {
+						if (otherOp.id === op.target) {
+							console.log("undoing", otherOp);
+							mutableMH.splice(mutableMH.indexOf(otherOp), 1);
+							break;
+						}
+					}
+				} else if (op.type === "recolor") {
+					for (const otherOp of mutableMH) {
+						if (otherOp.id === op.target) {
+							console.log("recoloring", otherOp);
+							otherOp.color = op.color;
+							break;
+						}
+					}
+				}
+				mutableMH.splice(mutableMH.indexOf(op), 1);
+			}
+		}
+	}
+	return mutableMH;
+}
+
+it("should resolve metahistory", () => {
+	expect(resolveMetaHistory([
+		{ id: "abc1", mhi: 0, type: "line", name: "Draw Line", color: "blue", },
+		{ id: "abc2", mhi: 1, type: "recolor", name: "Edit Draw Line", target: "abc1", color: "green" },
+		{ id: "abc3", mhi: 2, type: "undo", name: "Undo Edit Draw Line", target: "abc2" },
+		{ id: "abc4", mhi: 3, type: "undo", name: "Undo Undo Edit Draw Line", target: "abc3" }, // AKA Redo
+		{ id: "abc5", mhi: 0, type: "circle", name: "Draw Circle", color: "pink", },
+		{ id: "abc6", mhi: 0, type: "triangle", name: "Draw Triangle", color: "red", },
+		{ id: "abc7", mhi: 1, type: "undo", name: "Undo Draw Triangle", target: "abc6" },
+	])).toEqual([
+		{ id: "abc1", mhi: 0, type: "line", name: "Draw Line", color: "green", },
+		{ id: "abc5", mhi: 0, type: "circle", name: "Draw Circle", color: "pink", },
+	]);
+});
+
+
+function compute({ program, cache, goalNode }) {
+
+	resolveMetaHistory(program);
 
 }
 
 function deleteHistory({ program, cache, stepsToDelete }) {
 
+	// TODO: delete history, prereq: cache being populated?
 
 	// TODO: non-destructive (DDDBD), prereq: metahistory
 
