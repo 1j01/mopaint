edbaa92f13ec90380d325e97dbec0b4220e5811e
Isaiah Odhner <isaiahodhner@gmail.com>
2023-03-19 12:39:00 -0400

    Get <App> "renders without crashing" test working

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,8 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+if (global.crypto) {
+	console.trace("global.crypto already exists; this code can be removed");
+} else {
+	var nodeCrypto = require('crypto');
+	global.crypto = {
+		getRandomValues: function (buffer) { return nodeCrypto.randomFillSync(buffer); }
+	};
+}
+
 /* global it:false */
 it("renders without crashing", () => {
 	const div = document.createElement("div");
-	ReactDOM.render(<App />, div);
+	ReactDOM.render(<App
+		documentID={"test-document-id"}
+		goToDocument={() => { throw new Error("goToDocument not expected in this test"); }}
+		createNewDocument={() => { throw new Error("createNewDocument not expected in this test"); }}
+		loadNewDocument={() => { throw new Error("loadNewDocument not expected in this test"); }}
+		serializedDocumentToLoad={null}
+	/>, div);
 });
