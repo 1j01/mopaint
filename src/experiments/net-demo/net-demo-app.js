6020217408a58af9e9c91c2f8b2fbb0c043e3cd2
Isaiah Odhner <isaiahodhner@gmail.com>
2025-01-02 23:56:37 -0500

    Sync continuous operations (brush strokes)

--- src/experiments/net-demo/net-demo-app.js
+++ src/experiments/net-demo/net-demo-app.js
@@ -1,3 +1,4 @@
+import { generateID } from "../../helpers.js";
 import { Client, MopaintWebSocketClient } from "../networking.js";
 
 const client = new Client();
@@ -22,24 +23,53 @@ function getCursorPoint(event) {
 	return tempPoint.matrixTransform(svg.getScreenCTM().inverse());
 }
 
+let activeOperation = null;
 svg.addEventListener("pointerdown", (event) => {
 	const point = getCursorPoint(event);
-	client.addOperation({
-		type: "pointerdown",
-		x: point.x,
-		y: point.y,
+	activeOperation = {
+		operationId: generateID(),
+		type: "brush",
+		points: [{ x: point.x, y: point.y }],
 		color: `hsl(${Math.random() * 360}, 100%, 50%)`,
-	});
+	};
+	client.addOperation(activeOperation);
+
+	const pointerMoveListener = (event) => {
+		const point = getCursorPoint(event);
+		client.pushContinuousOperationData(activeOperation.operationId, {
+			points: { x: point.x, y: point.y },
+		});
+	};
+	const pointerUpListener = () => {
+		document.removeEventListener("pointermove", pointerMoveListener);
+		document.removeEventListener("pointerup", pointerUpListener);
+		activeOperation = null;
+	};
+	document.addEventListener("pointermove", pointerMoveListener);
+	document.addEventListener("pointerup", pointerUpListener);
 });
 
 client.onAnyOperation((operation) => {
-	if (operation.type === "pointerdown") {
+	if (operation.type === "circle") {
 		const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
 		circle.setAttribute("cx", operation.x);
 		circle.setAttribute("cy", operation.y);
 		circle.setAttribute("r", 10);
 		circle.setAttribute("fill", operation.color);
 		svg.appendChild(circle);
+	} else if (operation.type === "brush") {
+		const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+		path.setAttribute("stroke", operation.color);
+		path.setAttribute("fill", "none");
+		path.setAttribute("d", `M ${operation.points.map((point) => `${point.x} ${point.y}`).join(" L ")}`);
+		svg.appendChild(path);
+		// HACK: TODO: don't add so many listeners indefinitely
+		client.onAnyOperationUpdated((updatedOperation, data) => {
+			if (updatedOperation.operationId === operation.operationId) {
+				// operation.points.push(data.points); // already done by the Client class, I think
+				path.setAttribute("d", `M ${operation.points.map((point) => `${point.x} ${point.y}`).join(" L ")}`);
+			}
+		});
 	}
 });
 
--- src/experiments/networking.js
+++ src/experiments/networking.js
@@ -47,10 +47,16 @@ export class Client {
 		this.clientId = clientId ?? nextClientId++;
 		/** @type {Operation[]} */
 		this.metaHistory = [];
+
+		// TODO: make events less verbose using EventTarget or something
 		/** @type {((operation: Operation) => void)[]} */
 		this.localOperationListeners = [];
 		/** @type {((operation: Operation) => void)[]} */
 		this.anyOperationListeners = [];
+		/** @type {((operation: Operation, data: Record<string, {x: number, y: number}[]>) => void)[]} */
+		this.localOperationUpdatedListeners = [];
+		/** @type {((operation: Operation, data: Record<string, {x: number, y: number}[]>) => void)[]} */
+		this.anyOperationUpdatedListeners = [];
 	}
 
 	computeLinearHistory() {
@@ -94,6 +100,44 @@ export class Client {
 		}
 	}
 
+	pushContinuousOperationData(operationId, data, remote = false) {
+		// I feel like I'm overstepping the bounds of what consists as an "operation", or rather,
+		// that these continuously appended buffers could be better divorced from the concept of an operation, for future use cases and/or clarity.
+		// I may even be able to treat the operations list and the brush stroke data similarly, if I structure it so,
+		// both being append-only lists (in general, at least), and could potentially simplify the system that I'm developing.
+		// It would basically mean adding indirection to the operation's continuously updatable data buffer reference.
+		// It might be like a buffer ID, instead of using the operation's ID + a top-level key to identify the buffer for updating across the network.
+		//
+		// That said, the reason to have a separate buffer in the first place isn't a fundamental one, but rather for performance:
+		// If for every update it sent either the whole operation or an "update operation", the overhead of the operation objects would be significant.
+		// So if it's better for ergonomics to deal with whole operation objects and important for performance to use ArrayBuffer objects for stroke data,
+		// it may complicate a general system, and abstractions can have a performance cost as well.
+		//
+		// That said again, not all operations may want to pack data in the same way, so the abstraction may be necessary anyway,
+		// and it may indeed be simpler, so it's worth exploring.
+
+		// TODO: use a Map to look up the operation by ID in one step
+		const operation = this.metaHistory.find((op) => op.operationId === operationId);
+		if (!operation) {
+			console.error("Operation not found:", operationId);
+			return;
+		}
+		// TODO: record timestamp of each sample
+		// Also, this is pretty informal right now, just updating arbitrary keys in the operation object (and assuming they're arrays).
+		for (let key in data) {
+			operation[key].push(data[key]);
+		}
+
+		if (!remote) {
+			for (const listener of this.localOperationUpdatedListeners) {
+				listener(operation, data);
+			}
+		}
+		for (const listener of this.anyOperationUpdatedListeners) {
+			listener(operation, data);
+		}
+	}
+
 	/**
 	 * Listen for operations generated from this client.
 	 * @param {(operation: Operation) => void} listener
@@ -115,6 +159,30 @@ export class Client {
 		this.anyOperationListeners.push(listener);
 		return () => {
 			this.anyOperationListeners = this.anyOperationListeners.filter((fn) => fn !== listener);
+		};
+	}
+
+	/**
+	 * Listen for operations from this client being updated.
+	 * @param {(operation: Operation) => void} listener
+	 * @returns {() => void} function to remove the listener
+	 */
+	onLocalOperationUpdated(listener) {
+		this.localOperationUpdatedListeners.push(listener);
+		return () => {
+			this.localOperationUpdatedListeners = this.localOperationUpdatedListeners.filter((fn) => fn !== listener);
+		};
+	}
+
+	/**
+	 * Listen for operations from this client or other clients being updated.
+	 * @param {(operation: Operation) => void} listener
+	 * @returns {() => void} function to remove the listener
+	 */
+	onAnyOperationUpdated(listener) {
+		this.anyOperationUpdatedListeners.push(listener);
+		return () => {
+			this.anyOperationUpdatedListeners = this.anyOperationUpdatedListeners.filter((fn) => fn !== listener);
 		};
 	}
 }
@@ -176,18 +244,36 @@ export class MopaintWebSocketClient {
 				console.error("Received non-string message:", event.data);
 				return;
 			}
-			const operation = JSON.parse(event.data);
-			this.client.addOperation(operation);
+			const message = JSON.parse(event.data);
+			if (message.type === "operation") {
+				this.client.addOperation(message.operation);
+			} else if (message.type === "operationUpdate") {
+				this.client.pushContinuousOperationData(message.operationId, message.data, true);
+			}
 		});
 
+		// TODO: DRY
 		this.client.onLocalOperation((operation) => {
 			// Send local operations to the server
+			const message = JSON.stringify({ type: "operation", operation });
 			if (this.ws.readyState === WebSocket.OPEN) {
 				console.log("Sending operation to server:", operation);
-				this.ws.send(JSON.stringify(operation));
+				this.ws.send(message);
 			} else {
 				console.log("WebSocket not open, queueing message for operation:", operation);
-				pendingMessages.push(JSON.stringify(operation));
+				pendingMessages.push(message);
+			}
+		});
+
+		this.client.onLocalOperationUpdated((operation, data) => {
+			// Send local operation updates to the server
+			const message = JSON.stringify({ type: "operationUpdate", operationId: operation.operationId, data });
+			if (this.ws.readyState === WebSocket.OPEN) {
+				console.log("Sending operation update to server:", operation, data);
+				this.ws.send(message);
+			} else {
+				console.log("WebSocket not open, queueing message for operation update:", operation, data);
+				pendingMessages.push(message);
 			}
 		});
 	}
