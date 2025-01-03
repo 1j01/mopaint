b1e4214012ec4e5c2501c9b58a90959da770daa8
Isaiah Odhner <isaiahodhner@gmail.com>
2025-01-03 01:57:04 -0500

    Prevent scrolling with touch

--- src/experiments/net-demo/net-demo-app.js
+++ src/experiments/net-demo/net-demo-app.js
@@ -9,6 +9,7 @@ const root = document.getElementById("root");
 const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
 svg.setAttribute("width", "100%");
 svg.setAttribute("height", "100%");
+svg.style.touchAction = "none";
 root.appendChild(svg);
 
 let tempPoint = svg.createSVGPoint();
