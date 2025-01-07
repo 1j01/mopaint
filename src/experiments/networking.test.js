2dbedf1db38da7211d61b2eb5dc7fef72eb1ffec
Isaiah Odhner <isaiahodhner@gmail.com>
2025-01-07 18:12:23 -0500

    update tests

--- src/experiments/networking.test.js
+++ src/experiments/networking.test.js
@@ -8,16 +8,16 @@ describe("Client + InProcessPeerParty", () => {
 		const party = new InProcessPeerParty();
 		party.addPeer(clientA);
 		party.addPeer(clientB);
-		clientA.addOperation({ id: "abc1", metaLevel: 0, type: "line", name: "Draw Line", color: "blue", timestamp: 0 });
-		clientB.addOperation({ id: "abc2", metaLevel: 1, type: "recolor", name: "Edit Draw Line", target: "abc1", color: "green", timestamp: 1 });
+		clientA.addOperation({ operationId: "abc1", data: { metaLevel: 0, type: "line", name: "Draw Line", color: "blue" }, timestamp: 0 });
+		clientB.addOperation({ operationId: "abc2", data: { metaLevel: 1, type: "recolor", name: "Edit Draw Line", target: "abc1", color: "green" }, timestamp: 1 });
 		expect(clientA.metaHistory).toEqual([
-			{ clientId: 1, id: "abc1", metaLevel: 0, type: "line", name: "Draw Line", color: "blue", timestamp: 0 },
-			{ clientId: 2, id: "abc2", metaLevel: 1, type: "recolor", name: "Edit Draw Line", target: "abc1", color: "green", timestamp: 1 },
+			{ clientId: 1, operationId: "abc1", data: { metaLevel: 0, type: "line", name: "Draw Line", color: "blue" }, timestamp: 0 },
+			{ clientId: 2, operationId: "abc2", data: { metaLevel: 1, type: "recolor", name: "Edit Draw Line", target: "abc1", color: "green" }, timestamp: 1 },
 		]);
 		expect(clientB.metaHistory).toEqual(clientA.metaHistory);
 
 		// expect(clientA.computeLinearHistory()).toEqual([
-		// 	{ id: "abc1", metaLevel: 0, type: "line", name: "Draw Line", color: "green", },
+		// 	{ operationId: "abc1", data: { metaLevel: 0, type: "line", name: "Draw Line", color: "green", }},
 		// ]);
 		party.dispose();
 	});
@@ -27,11 +27,11 @@ describe("Client + InProcessPeerParty", () => {
 		const party = new InProcessPeerParty();
 		party.addPeer(clientA);
 		party.addPeer(clientB);
-		clientA.addOperation({ id: "abc1", metaLevel: 0, type: "line", name: "Draw Line", color: "blue", timestamp: 1 });
-		clientB.addOperation({ id: "abc2", metaLevel: 0, type: "line", name: "Draw Line", color: "yellow", timestamp: 0 });
+		clientA.addOperation({ operationId: "abc1", data: { metaLevel: 0, type: "line", name: "Draw Line", color: "blue" }, timestamp: 1 });
+		clientB.addOperation({ operationId: "abc2", data: { metaLevel: 0, type: "line", name: "Draw Line", color: "yellow" }, timestamp: 0 });
 		expect(clientA.metaHistory).toEqual([
-			{ clientId: 2, id: "abc2", metaLevel: 0, type: "line", name: "Draw Line", color: "yellow", timestamp: 0 },
-			{ clientId: 1, id: "abc1", metaLevel: 0, type: "line", name: "Draw Line", color: "blue", timestamp: 1 },
+			{ clientId: 2, operationId: "abc2", data: { metaLevel: 0, type: "line", name: "Draw Line", color: "yellow" }, timestamp: 0 },
+			{ clientId: 1, operationId: "abc1", data: { metaLevel: 0, type: "line", name: "Draw Line", color: "blue" }, timestamp: 1 },
 		]);
 		expect(clientB.metaHistory).toEqual(clientA.metaHistory);
 		party.dispose();
@@ -42,15 +42,15 @@ describe("Client + InProcessPeerParty", () => {
 		const party = new InProcessPeerParty();
 		party.addPeer(clientA);
 		party.addPeer(clientB);
-		clientA.addOperation({ id: "abc1", metaLevel: 0, type: "line", name: "Draw Line", color: "blue", timestamp: 1 });
-		clientB.addOperation({ id: "abc2", metaLevel: 0, type: "line", name: "Draw Line", color: "yellow", timestamp: 1 });
-		clientB.addOperation({ id: "abc3", metaLevel: 0, type: "line", name: "Draw Line", color: "green", timestamp: 1 });
-		clientA.addOperation({ id: "abc4", metaLevel: 0, type: "line", name: "Draw Line", color: "red", timestamp: 1 });
+		clientA.addOperation({ operationId: "abc1", data: { metaLevel: 0, type: "line", name: "Draw Line", color: "blue" }, timestamp: 1 });
+		clientB.addOperation({ operationId: "abc2", data: { metaLevel: 0, type: "line", name: "Draw Line", color: "yellow" }, timestamp: 1 });
+		clientB.addOperation({ operationId: "abc3", data: { metaLevel: 0, type: "line", name: "Draw Line", color: "green" }, timestamp: 1 });
+		clientA.addOperation({ operationId: "abc4", data: { metaLevel: 0, type: "line", name: "Draw Line", color: "red" }, timestamp: 1 });
 		expect(clientA.metaHistory).toEqual([
-			{ clientId: 1, id: "abc1", metaLevel: 0, type: "line", name: "Draw Line", color: "blue", timestamp: 1 },
-			{ clientId: 1, id: "abc4", metaLevel: 0, type: "line", name: "Draw Line", color: "red", timestamp: 1 },
-			{ clientId: 2, id: "abc2", metaLevel: 0, type: "line", name: "Draw Line", color: "yellow", timestamp: 1 },
-			{ clientId: 2, id: "abc3", metaLevel: 0, type: "line", name: "Draw Line", color: "green", timestamp: 1 },
+			{ clientId: 1, operationId: "abc1", data: { metaLevel: 0, type: "line", name: "Draw Line", color: "blue" }, timestamp: 1 },
+			{ clientId: 1, operationId: "abc4", data: { metaLevel: 0, type: "line", name: "Draw Line", color: "red" }, timestamp: 1 },
+			{ clientId: 2, operationId: "abc2", data: { metaLevel: 0, type: "line", name: "Draw Line", color: "yellow" }, timestamp: 1 },
+			{ clientId: 2, operationId: "abc3", data: { metaLevel: 0, type: "line", name: "Draw Line", color: "green" }, timestamp: 1 },
 		]);
 		expect(clientB.metaHistory).toEqual(clientA.metaHistory);
 		party.dispose();
@@ -61,18 +61,18 @@ describe("Client + InProcessPeerParty", () => {
 		const party = new InProcessPeerParty();
 		party.addPeer(clientA);
 		party.addPeer(clientB);
-		clientA.addOperation({ operationId: "abc1", metaLevel: 0, type: "brush", name: "Brush", points: [{ x: 5, y: 5 }], timestamp: 0 });
+		clientA.addOperation({ operationId: "abc1", data: { metaLevel: 0, type: "brush", name: "Brush", points: [{ x: 5, y: 5 }] }, timestamp: 0 });
 		clientA.pushContinuousOperationData("abc1", { points: { x: 10, y: 10 } });
 		expect(clientA.metaHistory).toEqual([
-			{ clientId: 1, operationId: "abc1", metaLevel: 0, type: "brush", name: "Brush", points: [{ x: 5, y: 5 }, { x: 10, y: 10 }], timestamp: 0 },
+			{ clientId: 1, operationId: "abc1", data: { metaLevel: 0, type: "brush", name: "Brush", points: [{ x: 5, y: 5 }, { x: 10, y: 10 }] }, timestamp: 0 },
 		]);
 		expect(clientB.metaHistory).not.toBe(clientA.metaHistory); // must not cheat by copying the reference
 		expect(clientB.metaHistory[0]).not.toBe(clientA.metaHistory[0]); // must not cheat by copying the reference
-		expect(clientB.metaHistory[0].points[1]).not.toBe(clientA.metaHistory[0].points[1]); // must not cheat by copying the reference
+		expect(clientB.metaHistory[0].data.points[1]).not.toBe(clientA.metaHistory[0].data.points[1]); // must not cheat by copying the reference
 		expect(clientB.metaHistory).toEqual(clientA.metaHistory); // should match though
 
 		// expect(clientA.computeLinearHistory()).toEqual([
-		// 	{ id: "abc1", metaLevel: 0, type: "line", name: "Draw Line", color: "green", },
+		// 	{ operationId: "abc1", data: { metaLevel: 0, type: "line", name: "Draw Line", color: "green", }},
 		// ]);
 		party.dispose();
 	});
@@ -115,12 +115,12 @@ describe("Client + MopaintWebSocketServer + MopaintWebSocketClient", () => {
 			// TODO: dispose of these clients
 			new MopaintWebSocketClient(clientA, "ws://localhost:8283");
 			new MopaintWebSocketClient(clientB, "ws://localhost:8283");
-			clientA.addOperation({ id: "abc1", metaLevel: 0, type: "line", name: "Draw Line", color: "blue", timestamp: 0 });
-			clientB.addOperation({ id: "abc2", metaLevel: 1, type: "recolor", name: "Edit Draw Line", target: "abc1", color: "green", timestamp: 1 });
+			clientA.addOperation({ operationId: "abc1", data: { metaLevel: 0, type: "line", name: "Draw Line", color: "blue" }, timestamp: 0 });
+			clientB.addOperation({ operationId: "abc2", data: { metaLevel: 1, type: "recolor", name: "Edit Draw Line", target: "abc1", color: "green" }, timestamp: 1 });
 			await waitForSynchronization([clientA, clientB], 2);
 			expect(clientA.metaHistory).toEqual([
-				{ clientId: 1, id: "abc1", metaLevel: 0, type: "line", name: "Draw Line", color: "blue", timestamp: 0 },
-				{ clientId: 2, id: "abc2", metaLevel: 1, type: "recolor", name: "Edit Draw Line", target: "abc1", color: "green", timestamp: 1 },
+				{ clientId: 1, operationId: "abc1", data: { metaLevel: 0, type: "line", name: "Draw Line", color: "blue" }, timestamp: 0 },
+				{ clientId: 2, operationId: "abc2", data: { metaLevel: 1, type: "recolor", name: "Edit Draw Line", target: "abc1", color: "green" }, timestamp: 1 },
 			]);
 			expect(clientB.metaHistory).toEqual(clientA.metaHistory);
 		} finally {
@@ -135,12 +135,12 @@ describe("Client + MopaintWebSocketServer + MopaintWebSocketClient", () => {
 			// TODO: dispose of these clients
 			new MopaintWebSocketClient(clientA, "ws://localhost:8283");
 			new MopaintWebSocketClient(clientB, "ws://localhost:8283");
-			clientA.addOperation({ id: "abc1", metaLevel: 0, type: "line", name: "Draw Line", color: "blue", timestamp: 0 });
-			clientB.addOperation({ id: "abc2", metaLevel: 1, type: "recolor", name: "Edit Draw Line", target: "abc1", color: "green", timestamp: 1 });
+			clientA.addOperation({ operationId: "abc1", data: { metaLevel: 0, type: "line", name: "Draw Line", color: "blue" }, timestamp: 0 });
+			clientB.addOperation({ operationId: "abc2", data: { metaLevel: 1, type: "recolor", name: "Edit Draw Line", target: "abc1", color: "green" }, timestamp: 1 });
 			await waitForSynchronization([clientA, clientB], 2);
 			expect(clientA.metaHistory).toEqual([
-				{ clientId: 1, id: "abc1", metaLevel: 0, type: "line", name: "Draw Line", color: "blue", timestamp: 0 },
-				{ clientId: 1, id: "abc2", metaLevel: 1, type: "recolor", name: "Edit Draw Line", target: "abc1", color: "green", timestamp: 1 },
+				{ clientId: 1, operationId: "abc1", data: { metaLevel: 0, type: "line", name: "Draw Line", color: "blue" }, timestamp: 0 },
+				{ clientId: 1, operationId: "abc2", data: { metaLevel: 1, type: "recolor", name: "Edit Draw Line", target: "abc1", color: "green" }, timestamp: 1 },
 			]);
 			expect(clientB.metaHistory).toEqual(clientA.metaHistory);
 		} finally {
@@ -155,16 +155,16 @@ describe("Client + MopaintWebSocketServer + MopaintWebSocketClient", () => {
 			// TODO: dispose of these clients
 			new MopaintWebSocketClient(clientA, "ws://localhost:8283");
 			new MopaintWebSocketClient(clientB, "ws://localhost:8283");
-			clientA.addOperation({ operationId: "abc1", metaLevel: 0, type: "brush", name: "Brush", points: [{ x: 5, y: 5 }], timestamp: 0 });
+			clientA.addOperation({ operationId: "abc1", data: { metaLevel: 0, type: "brush", name: "Brush", points: [{ x: 5, y: 5 }] }, timestamp: 0 });
 			clientA.pushContinuousOperationData("abc1", { points: { x: 10, y: 10 } });
 			// is there a race condition here? should I wait for the histories to deeply match, not just their lengths?
 			await waitForSynchronization([clientA, clientB], 1);
 			expect(clientA.metaHistory).toEqual([
-				{ clientId: 1, operationId: "abc1", metaLevel: 0, type: "brush", name: "Brush", points: [{ x: 5, y: 5 }, { x: 10, y: 10 }], timestamp: 0 },
+				{ clientId: 1, operationId: "abc1", data: { metaLevel: 0, type: "brush", name: "Brush", points: [{ x: 5, y: 5 }, { x: 10, y: 10 }] }, timestamp: 0 },
 			]);
 			expect(clientB.metaHistory).not.toBe(clientA.metaHistory); // cheating here should be impossible, can't copy references across the network
 			expect(clientB.metaHistory[0]).not.toBe(clientA.metaHistory[0]); // cheating here should be impossible, can't copy references across the network
-			expect(clientB.metaHistory[0].points[1]).not.toBe(clientA.metaHistory[0].points[1]); // cheating here should be impossible, can't copy references across the network
+			expect(clientB.metaHistory[0].data.points[1]).not.toBe(clientA.metaHistory[0].data.points[1]); // cheating here should be impossible, can't copy references across the network
 			expect(clientB.metaHistory).toEqual(clientA.metaHistory);
 		} finally {
 			server.dispose();
